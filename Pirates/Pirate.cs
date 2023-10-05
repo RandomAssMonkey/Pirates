73c3c5dfe025a36c68fbf405eba6e585ef1bed54
Petrik Diak <diak@petrik.hu>
2023-10-05 11:20:12 +0200

    Add project files.

---
+++ Pirates/Pirate.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pirates
+{
+	internal class Pirate
+	{
+		private string crewmate;
+		private int toxicated;
+
+		public string Crewmate { get => crewmate; set => crewmate = value; }
+		public int Toxicated { get => toxicated; set => toxicated = value; }
+
+		public Pirate(string crewmate)
+		{
+			this.crewmate = crewmate;
+			this.toxicated = 0;
+		}
+
+		public int drinkSomeRum()
+		{
+			this.toxicated++;
+			return this.toxicated;
+		}
+
+		public int GetToxicated()
+		{
+			return toxicated;
+		}
+
+		public string howItsGoingMate()
+		{
+			if (this.toxicated >= 4)
+			{
+				this.Toxicated = 0;
+				return "Passes out";
+			}
+			else
+			{
+				return "Pour me anudder!";
+			}
+		}
+
+
+
+
+	}
+}
---
+++ Pirates/Program.cs
@@ -0,0 +1,45 @@
+﻿using Pirates;
+
+List<Pirate> pirates = new List<Pirate>();
+
+Pirate p1 = new Pirate("Zoro");
+Pirate p2 = new Pirate("Luffy");
+Pirate p3 = new Pirate("Usopp");
+Pirate p4 = new Pirate("Nami");
+Pirate p5 = new Pirate("Gol D. Roger");
+
+pirates.Add(p1);
+pirates.Add(p2);
+pirates.Add(p3);
+pirates.Add(p4);
+pirates.Add(p5);
+
+for (int i = 0; i < 30; i++)
+{
+	Random rnd = new Random();
+	int drink = rnd.Next(1,6);
+	switch (drink)
+	{
+		case 1:
+			p1.drinkSomeRum();
+			break;
+		case 2:
+			p2.drinkSomeRum();
+			break;
+		case 3:
+			p3.drinkSomeRum();
+			break;
+		case 4:
+			p4.drinkSomeRum();
+			break;
+		case 5:
+			p5.drinkSomeRum();
+			break;
+	}
+}
+for (int i = 0; i < pirates.Count; i++)
+{
+	Console.WriteLine(pirates[i].howItsGoingMate());
+}
+Console.ReadKey();
+
